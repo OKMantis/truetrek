fb66e1b68080a03ec42c748bbd00393e8220e76f
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-12-01 17:03:59 +0100

    added routes and extra attributes user to application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,46 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "cities#index"
+
+  resources :cities, only: :index do
+    resources :places, only: :index
+  end
+
+  resources :places, only: :index do 
+    resources :travel_book_places, only: :create
+  end
+
+  resources :travel_book_places, only: :destroy
+  resources :travel_books, only: :show
+  resources :comments, only: [:new, :create]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "posts#index"
