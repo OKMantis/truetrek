81ee3196fe60385054254f57e7c8dd96855dcb28
eyoab habtamu <eyoabhabtamu20@gmail.com>
2025-12-02 11:31:13 +0100

    The Geocoding gem is created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_01_154020) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_02_102121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_01_154020) do
     t.bigint "city_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["city_id"], name: "index_places_on_city_id"
   end
 
