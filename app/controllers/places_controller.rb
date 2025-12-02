f4508206fbe3d9ffe5b864a64aab1ea398428912
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-12-02 15:32:28 +0100

    set authorization for show page place, added multiple: true to new comment form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,18 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: [:show]
 
   def index
     @places = policy_scope(Place)
   end
+
+  def show
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+    authorize @place
+  end
+
 end
