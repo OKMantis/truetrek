38e172a0fa68835c8115b015af1cf46d7616bb2d
egor kanunnikov <ek@mrm.works>
2025-12-03 15:01:03 +0100

    pg search

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,16 @@
 class Comment < ApplicationRecord
+  include PgSearch::Model
+
   belongs_to :place
   belongs_to :user
   has_many_attached :photos
 
   validates :description, presence: true
   validates :description, length: { minimum: 20 }
-  
+
+  pg_search_scope :search,
+    against: [:description],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
