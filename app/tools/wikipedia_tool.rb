f431e0f3ad3870827c02e31628e6b866d3725c37
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-12-03 17:22:20 +0100

    finished tool

--- app/tools/wikipedia_tool.rb
+++ app/tools/wikipedia_tool.rb
@@ -1,11 +1,11 @@
 require 'open-uri'
 class WikipediaTool < RubyLLM::Tool
   description "Gets a summary of the place"
-  param :place_name "name of the place"
+  param :place_name, desc: "name of the place"
   
 
-  def execute(:place_name)
-    url = "https://en.wikipedia.org/api/rest_v1/page/summary/#{:place_name}"
+  def execute(place_name:)
+    url = "https://en.wikipedia.org/api/rest_v1/page/summary/#{place_name}"
 
     response = URI.parse(url).read
     JSON.parse(response)
